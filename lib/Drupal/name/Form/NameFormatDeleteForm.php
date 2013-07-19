2b13f109ef4a2576a35bb6cea8eeb7298d5693ac
Florian Weber <florian.weber@ueberbit.de>
2013-07-19 23:01:36 +0200

    Implement NameFormatDeleteForm.

---
+++ lib/Drupal/name/Form/NameFormatDeleteForm.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\name\Form\NameFormatDeleteForm.
+ */
+
+namespace Drupal\name\Form;
+use Drupal\Core\Entity\EntityConfirmFormBase;
+
+/**
+ * Builds the form to delete a name format.
+ */
+class NameFormatDeleteForm extends EntityConfirmFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getQuestion() {
+    return t('Are you sure you want to delete the custom format %name?', array('%name' => $this->entity->label()));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCancelPath() {
+    return 'admin/config/regional/name';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConfirmText() {
+    return t('Delete');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submit(array $form, array &$form_state) {
+    $this->entity->delete();
+    drupal_set_message(t('The custom name format %label has been deleted.', array('%label' => $this->entity->label())));
+    $form_state['redirect'] = $this->getCancelPath();
+  }
+
+}
